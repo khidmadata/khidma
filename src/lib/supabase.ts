656983ab62895aa03d661f0769d5b9cb4a9cb036
SherifLawyer <sharifhesham@hotmail.com>
2026-02-23 19:28:44 +0200

    Initial Khidma setup

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://icureesvnphgoxyrogxn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljdXJlZXN2bnBoZ294eXJvZ3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODgxMDksImV4cCI6MjA4NzM2NDEwOX0.-XSxNbnrQaPOBIM_RYEw57U0KzbMKqB5rOlBwNcQVQw'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
